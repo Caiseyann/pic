647c27bf450d1d7777e13edb9e6766be22afa1db
caiseyann <caiseyann4@gmail.com>
2020-10-10 01:18:12 +0300

    importing modules for views.py in portfolio folder

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
+# from django.shortcuts import render
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt
+from .models import Image
 
 # Create your views here.
