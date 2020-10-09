0627aede26fa10bd3b468f429fd12f880e971c51
caiseyann <caiseyann4@gmail.com>
2020-10-10 01:47:18 +0300

    created class category

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 import datetime as dt
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        self.update_category()
