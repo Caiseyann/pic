0942bd18470e4ed145a65bf4f8bd5e3c66780d71
caiseyann <caiseyann4@gmail.com>
2020-10-10 01:41:17 +0300

    creating instance return render

--- portfolio/views.py
+++ portfolio/views.py
@@ -31,3 +31,11 @@ def search_results(request):
         search_term = request.GET.get("image")
         searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
+
+
+        ## render a HTML template and pass in the list of items found and the search_term.
+        return render(request, 'all-folios/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request,'all-folios/search.html',{"message":message})
