4f081c8b07b130b780ed379d429bca835480a2d6
caiseyann <caiseyann4@gmail.com>
2020-10-10 01:28:08 +0300

    defined function photo_category

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,11 @@ import datetime as dt
 from .models import Image
 
 # Create your views here.
+
+
+
+# displaying different photo categories
+def photo_category(request):
+    date = dt.date.today()## current date
+    portfolio = Image.objects.all
+    return render(request, 'all-folios/category.html', {"date": date, "portfolio": portfolio})
