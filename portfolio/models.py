ac266b6412f22bd87e5eb45e747733e140fa6fd6
caiseyann <caiseyann4@gmail.com>
2020-10-10 01:47:54 +0300

    created class location

--- portfolio/models.py
+++ portfolio/models.py
@@ -16,3 +16,24 @@ class Category(models.Model):
 
     def update_category(self):
         self.update_category()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self):
+        self.update_location()
+
+    @classmethod
+    def get_location(cls):
+        location = cls.objects.all()
+        return location
