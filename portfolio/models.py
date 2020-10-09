e072ecb05c8fdc2cabcdb729fad20b4761746a23
caiseyann <caiseyann4@gmail.com>
2020-10-10 01:51:07 +0300

    created class image

--- portfolio/models.py
+++ portfolio/models.py
@@ -37,3 +37,48 @@ class Location(models.Model):
     def get_location(cls):
         location = cls.objects.all()
         return location
+
+
+class Image(models.Model):
+    title = models.CharField(max_length = 30)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    posted_date= models.DateTimeField(auto_now_add=True)
+    ## defines where the image will be stored in the file system.
+    category_image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title
+
+  
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update_image()
+
+##  method that will query the database and fetch our results.
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        portfolio = cls.objects.filter(category__name__icontains=search_term)
+        return portfolio
+
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
+
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        img_id = cls.objects.get(pk=id)
+        return img_id
+
+    # class Meta:
+    #     ordering = ['first_name']
