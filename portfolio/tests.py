b0c7ee3ce7268446fcda1aa64f4b4e0657c500f4
caiseyann <caiseyann4@gmail.com>
2020-10-10 02:02:42 +0300

    test for class image

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -19,3 +19,26 @@ class CategoryTestClass(TestCase):
 
     def setUp(self):
         pass
+
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        pass
+
+        # Creating a new category and saving it
+        self.new_category = Category(name='birds')
+        self.new_category.save()
+
+        # Creating a new location and saving it
+        self.new_location = Location(name='nakuru')
+        self.new_location.save()
+
+        # Creating new Image and saving it
+        self.new_image = Image(title='flamigo',
+        description='The pink flamigo')
+        self.new_image.save()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
