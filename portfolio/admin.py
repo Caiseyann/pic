6d00fe98e7fa89dfd8d287c44153966e8e6f3640
caiseyann <caiseyann4@gmail.com>
2020-10-10 01:52:04 +0300

    configured admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Location,Category,Image
+
+
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal =('title',)
+
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
