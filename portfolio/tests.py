aaa4ed950861d3c693a43e6533366945abb33083
caiseyann <caiseyann4@gmail.com>
2020-10-10 02:01:12 +0300

    test for class location

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -2,3 +2,11 @@ from django.test import TestCase
 from .models import Image,Location,Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+
+    """
+    A test that checks for the
+    """
+
+    def setUp(self):
+        pass
